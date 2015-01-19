826600ef9562337a12d959a56a882385b4be3a70
ewatch <ewatchhu@gmx.de>
2015-01-20 00:08:26 +0100

    create patched file

--- DiffPatcher.cs
+++ DiffPatcher.cs
@@ -11,13 +11,17 @@ namespace StrifeModManager {
 
         public static void makeChangesToFile() {
             diff_match_patch diffPatcher = new diff_match_patch();
-            string fileContent1 = readFile(@"C:\ModManager\mods\game_1.interface");
-            string fileContent2 = readFile(@"C:\ModManager\mods\game_2.interface");
+            string fileContent1 = readFile(@"C:\ModManager\mods\mod1.xml");
+            string fileContent2 = readFile(@"C:\ModManager\mods\mod2.xml");
             List<Diff> diffs = diffPatcher.diff_main(fileContent1, fileContent2);
             foreach (Diff aDiff in diffs) {
-                Console.WriteLine(aDiff.text);
+                Console.WriteLine(aDiff.operation + " " + aDiff.text);
             }
 
+            List<Patch> patches = diffPatcher.patch_make(fileContent1, diffs);
+            object[] applied = diffPatcher.patch_apply(patches, fileContent1);
+
+            writeFile(@"C:\ModManager\mods\mod3.xml", (string)applied[0]);
 
         }
 
@@ -27,6 +31,13 @@ namespace StrifeModManager {
              fileContent = sr.ReadToEnd();
              sr.Close();
              return fileContent;
+        }
+
+        private static void writeFile(string filename, string content) {
+            StreamWriter writer = new StreamWriter(filename);
+            writer.Write(content);
+            writer.Close();
+
         }
     }
 }
---
+++ Patcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrifeModManager {
+    class Patcher {
+        DiffMatchPatch.diff_match_patch diffPatcher = new DiffMatchPatch.diff_match_patch();
+
+        public void patch(string file1, string file2) {
+
+            diffPatcher.diff_main();
+        }
+    }
+}
